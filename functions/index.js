a8c448c862b501a968a9ee3c625a94773dee957b
d3xt3r-grv <grvarora3011@gmail.com>
2018-10-17 09:00:12 +0530

    find shops, add data done

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,17 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+
+
 admin.initializeApp(functions.config().firebase);
+
+app.get('/fdata',fdata);
+app.post('/addData',addData);
 
 var db = admin.firestore();
 db.settings({timestampsInSnapshots:true});
@@ -15,7 +26,7 @@ let collection = db.collection('shops');
 // Create a GeoFirestore index
 //const geoFirestore = new GeoFirestore(collectionRef);
 
-exports.fdata = functions.https.onRequest((req, res) => {
+function fdata(req, res){
 
 	console.log("hey");
 	const lat = parseFloat(req.query.dis)*0.0144927536231884;
@@ -36,9 +47,13 @@ console.log(doc.data().shopname+doc.data().longitude);
 						if(snapshot.exists){
 							console.log(doc.data().shopname+doc.data().longitude);
 							console.log('heyyyy');
-							console.log(doc.id, '=>', doc.data());
+							let dat={
+								price:snapshot.data().price,
+								data:doc.data()
+							}
+							console.log(doc.id, '=>', dat);
 							data.shop.push({
-								data:doc.data(),
+								data:dat,
 							});
 						}
 					}).catch(err => {console.log(err);})
@@ -58,23 +73,56 @@ console.log(doc.data().shopname+doc.data().longitude);
   // [END get_multiple]
 
 
-});
-exports.addData = functions.https.onRequest(function(req, res){
-	var data={
-		item:'lays',
-		price:20,
-		quantity:50
-	};
-	var ref= db.collection("shops").doc("icy spicy").collection("inventory").doc("food").collection("packed food");
-	ref.add(data);
-	ref= db.collection("shops").doc("icy spicy").collection("inventory").doc("food").collection("packed food").doc("lays");
-	//console.log(ref);
-	ref.get().then((snapshot)=>{
-		console.log(snapshot.data());
-	}).catch(err => {console.log(err);})
+}
+function addData(req, res){
+	// var data={
+	// 	item:'lays',
+	// 	price:20,
+	// 	quantity:50
+	// };
+	var data=req.body.itemdata;
+	var item=req.body.item;
+	console.log(data.shopname);
+	console.log(item);
+	var ref= db.collection("shops").doc(data.shopname).collection("inventory").doc(data.category).collection(data.subcategory).doc(data.item);
+	ref.set(item);
+	//ref.add(data);
+	// ref= db.collection("shops").doc("baba store").collection("inventory").doc("food").collection("packed food").doc("lays");
+	// //console.log(ref);
+	// ref.get().then((snapshot)=>{
+	// 	console.log(snapshot.data());
+	// }).catch(err => {console.log(err);})
 	return res.send('tan tana tan tan tan tara, chalti hai kya 9 se 12');
-});
+}
+function getAllItems(req, res){
 
-exports.returnShop = functions.https.onRequest(function(req, res){
+	console.log("hey");
+	const lat = parseFloat(req.query.dis)*0.0144927536231884;
+	const lon = parseFloat(req.query.dis)*0.0181818181818182;
+  	var citiesRef = db.collection('shops');
+console.log(parseFloat(req.query.latitude)+1+"   "+req.query.longitude);
+  var query = citiesRef.where('latitude', '>=', parseFloat(req.query.latitude)-lat).where('latitude', '<=', parseFloat(req.query.latitude)+lat).get()
+      .then(snapshot => {
 
-})
+      	let data = {shop:[]};
+        snapshot.forEach(doc => {
+console.log(doc.data().shopname+doc.data().longitude);
+          if(doc.data().longitude<=parseFloat(req.query.longitude)+lon &&doc.data().longitude>=parseFloat(req.query.longitude)-lon){
+					//////////////           CODE HERE              ///////////////////
+        } });
+		  res.json();
+      })
+      .catch(err => {
+
+
+        console.log('Error getting documents', err);
+
+     	return res.json({
+     		mes:"flas"
+     	})
+
+      });
+  // [END get_multiple]
+
+}
+exports.api = functions.https.onRequest(app);
